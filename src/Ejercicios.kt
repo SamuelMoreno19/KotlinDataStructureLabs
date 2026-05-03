dce943d6f74fe051a238db18ef4a1a7ec7842674
Samuel Moreno Carmona <morenosamuel2007@gmail.com>
2026-05-03 12:29:35 -0500

    12vo ejercicio resuelto

--- src/Ejercicios.kt
+++ src/Ejercicios.kt
@@ -150,3 +150,18 @@ fun reto11() {
     println("Reto 11: Organizador de Maleta")
     println("Objetos por peso: $porPeso")
 }
+
+// Ejercicio 12 - Verificador de Ruta Reversible
+fun reto12() {
+    val ruta = listOf(5 to 10, 3 to 3, 0 to 0, 3 to 3, 5 to 10)
+    var esEspejo = true
+    for (i in 0 until ruta.size / 2) {
+        if (ruta[i] != ruta[ruta.size - 1 - i]) {
+            esEspejo = false
+            break
+        }
+    }
+
+    println("Reto 12: Ruta Espejo")
+    println("¿Es espejo?: $esEspejo")
+}
